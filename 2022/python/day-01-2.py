5ab915e9d4a4826c4a8f8997ed8972167b89a418
Hugh Brown <hughdbrown@yahoo.com>
2023-12-01 17:31:24 -0700

    Add 2022 files

---
+++ 2022/python/day-01-2.py
@@ -0,0 +1,14 @@
+from heapq import heapify, heappush, heappop
+elves = [0]
+
+with open("input.txt") as handle:
+    for line in handle:
+        if (d := line.strip()):
+            elves[-1] += int(d)
+        else:
+            elves.append(0)
+
+neg_elves = [-h for h in elves]
+heapify(neg_elves)
+print(-heappop(neg_elves) + -heappop(neg_elves) + -heappop(neg_elves))
+
