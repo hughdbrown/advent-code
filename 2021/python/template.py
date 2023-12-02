8c5957d0aa795272a62e7dc5f4c4805be8fb2f38
Hugh Brown <hughdbrown@yahoo.com>
2023-12-01 17:31:13 -0700

    Add 2021 files

---
+++ 2021/python/template.py
@@ -0,0 +1,16 @@
+a = """
+"""
+
+data = [int(aa) for aa in a.splitlines()]
+
+
+def puzzle1():
+    return 0
+
+def puzzle2():
+    return 0
+
+
+if __name__ == '__main__':
+    print(puzzle1())
+    print(puzzle2())
